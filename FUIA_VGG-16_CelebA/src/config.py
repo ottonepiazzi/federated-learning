17da3882c80c4f1c283d846ddc28be11d0a72069
OttonePiazzi <ottonepiazzi@gmail.com>
2026-07-06 14:20:19 +0200

    Update config.py

--- FUIA_VGG-16_CelebA/src/config.py
+++ FUIA_VGG-16_CelebA/src/config.py
@@ -21,7 +21,6 @@ print(f"Device: {DEVICE}")
 
 
 
-#######################################################################################################
 #hyperparameters
 NUM_CLIENTS      = 50
 FRACTION         = 0.2         #20% client participation per round
@@ -40,9 +39,9 @@ IMG_CHANNELS     = 3           #RGB
 IMG_MEAN         = (0.5, 0.5, 0.5)
 IMG_STD          = (0.5, 0.5, 0.5)
 
-#Gradient inversion (paper Section V.B, Eq. 13)
+#Gradient inversion
 INV_ITERATIONS   = 10000
 INV_LR           = 0.01
 INV_ALPHA        = 1e-5         #TV regularization weight (paper Eq. 13)
 INV_RESTARTS     = 3
-#######################################################################################################
+
