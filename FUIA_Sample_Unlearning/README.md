5c8a41820a8b617e73559ee7cf2c048fba22b5d3
OttonePiazzi <ottonepiazzi@gmail.com>
2026-07-09 19:58:12 +0200

    refactoring

---
+++ FUIA_Sample_Unlearning/README.md
@@ -0,0 +1,32 @@
+# FUIA - Sample Unlearning (CNN on MNIST)
+
+Implementation of the FUIA (Federated Unlearning Inversion Attack) from the paper
+"Model Inversion Attack Against Federated Unlearning" (Zhou et al., IEEE TIFS 2026),
+sample unlearning scenario with Retraining on MNIST dataset.
+
+## Setup
+
+### 1. Create a virtual environment
+
+**macOS / Linux:**
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+**Windows:**
+```bash
+python -m venv env
+env\Scripts\activate
+pip install -r requirements.txt
+```
+
+### 2. Run
+
+```bash
+cd src
+python main.py
+```
+
+At the beginning of each run you are asked to log metrics on WandB. To skip it, insert "3".
---
+++ FUIA_Sample_Unlearning/src/unlearning.py
@@ -0,0 +1,26 @@
+import random
+
+from model import CNN
+from fl_training import run_fl_rounds
+
+
+def select_forgotten_samples(client_data, private_data):
+    #Randomly select 1 sample per client to forget (paper Sec VI.B.1)
+    forgotten = {}
+    for client_id, sample_indices in client_data.items():
+        forgotten[client_id] = random.choice(sample_indices)
+    return forgotten
+
+
+def retrain_without_samples(pretrained_sd, private_data, client_data,
+                            forgotten_samples, round_selections):
+    model = CNN()
+    model.load_state_dict(pretrained_sd)
+    modified_data = {}
+    for client_id, sample_indices in client_data.items():
+        if client_id in forgotten_samples:
+            modified_data[client_id] = [i for i in sample_indices
+                                        if i != forgotten_samples[client_id]]
+        else:
+            modified_data[client_id] = list(sample_indices)
+    return run_fl_rounds(model, modified_data, private_data, round_selections)
